ef3f706cd983cd6a3b4fa5b72d574a3533dd5139
Ajans Mobil <info@ajansmobil.com>
2026-01-12 05:13:02 +0000

    Edit

--- module/go1ilz/back.js
+++ module/go1ilz/back.js
@@ -1,2 +1,3 @@
-console.log("code bos",);
+// Çeviri sistemi - webmakerrender.js tarafından otomatik olarak @placeholder@ formatındaki metinler değiştirilir
+// HTML'deki @placeholder@ formatındaki metinler otomatik olarak lang.json'dan çevrilir
 html = html;
