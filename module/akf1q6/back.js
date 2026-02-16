4b3d8143daf02a94f99c249f3dc4e0159350308e
Ajans Mobil <info@ajansmobil.com>
2026-02-16 19:30:31 +0000

    Edit

--- module/akf1q6/back.js
+++ module/akf1q6/back.js
@@ -1,27 +1,16 @@
-
-
-var data = "";
-
-if (json.desing && json.desing.data) {
-    for (var key in json.desing.data) {
-        var item = json.desing.data[key];
-        var title = item.title && item.title[json.lang] ? item.title[json.lang] : item[json.lang] || 'Başlık';
-        
-        data += `
-        <div class="modulex-item" onclick="pageteklif('modulex-${item.id}')">
-            <img class="modulex-item-img" 
-                 src="/img/akf1q6/${item.img}" 
-                 alt="${title}"
-                 loading="lazy">
-            <div class="modulex-item-title">
-                ${title}
-            </div>
-        </div>`;
+// Dinamik menu: desing.data kartlarından menü HTML üretilir.
+// {{menu}} placeholder'ı render'da desing key'i olmadığı için back.js'de doldurulur.
+var menuHtml = '';
+if (typeof json !== 'undefined' && json.desing && json.desing.data && Array.isArray(json.desing.data)) {
+    var lang = (json.lang && json.lang === 'en') ? 'en' : 'tr';
+    for (var i = 0; i < json.desing.data.length; i++) {
+        var it = json.desing.data[i];
+        var title = (it.title && it.title[lang]) ? it.title[lang] : (it.title && it.title.tr) ? it.title.tr : (it.title && it.title.en) ? it.title.en : 'Kart ' + (i + 1);
+        var img = it.img ? ('/module/akf1q6/images/' + it.img) : '/module/akf1q6/images/kargo-tasimaciligi.jpg';
+        var modulexId = 'modulex-' + (i + 1);
+        menuHtml += '<div class="modulex-item" role="button" tabindex="0" onclick="pageteklif(\'' + modulexId + '\')" style="cursor: pointer;" aria-label="' + title + '">';
+        menuHtml += '<img class="modulex-item-img" src="' + img + '" alt="' + title + '" width="80" height="80" loading="lazy">';
+        menuHtml += '<span class="modulex-item-title">' + title + '</span></div>';
     }
 }
-
-html = html.replace(new RegExp("{{menu}}", "g"), data);
-
-
-
-
+html = html.replace(/\{\{menu\}\}/g, menuHtml || '');
