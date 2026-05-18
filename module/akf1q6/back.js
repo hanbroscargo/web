dbce4b5129c4caa4ed344f909ad5521d27150cda
Ajans Mobil <info@ajansmobil.com>
2026-05-18 06:56:46 +0000

    Edit

--- module/akf1q6/back.js
+++ module/akf1q6/back.js
@@ -23,7 +23,7 @@ for (var i = 0; i < TASIMA_TIPLERI.length; i++) {
     var iconName = (it && it.icon) ? it.icon : tip.icon;
     var hash = GO1ILZ_HASH[i] || 'zati';
     var href = go1ilzBase + '#' + hash;
-    menuHtml += '<a class="modulex-item" href="' + href + '" aria-label="' + title + '">';
+    menuHtml += '<a class="modulex-item" href="' + href + '" aria-label="' + title + '" data-request-type="' + tip.code + '">';
     menuHtml += '<span class="modulex-item-badge">' + tip.code + '</span>';
     menuHtml += '<span class="modulex-item-icon material-symbols-outlined" aria-hidden="true">' + iconName + '</span>';
     menuHtml += '<span class="modulex-item-title">' + title + '</span>';
